752170bc115662ad54a8a4f26b86d9122a213e11
alejandroruizdiaz <alee_ruizdiaz@hotmail.com>
2023-03-09 17:48:46 -0300

    Adding btn wps

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Footer from "./components/Footer/Footer";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Pages/Home/Home";
+import WhatsApp from "./components/WhatsApp/WhatsApp";
 
 function App() {
     return (
         <>
             <Navbar />
+            <WhatsApp/>
             <Routes>
                 <Route path="/" element={<Home />} />
             </Routes>
