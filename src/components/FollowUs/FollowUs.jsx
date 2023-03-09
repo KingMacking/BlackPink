b224e07e61765f529c45b3a68a1af86390699d3c
King Macking <matiasatzori@gmail.com>
2023-03-09 17:56:02 -0300

    Fixed navbar z-index

--- src/components/FollowUs/FollowUs.jsx
+++ src/components/FollowUs/FollowUs.jsx
@@ -6,9 +6,9 @@ const FollowUs = () => {
         <div className='flex flex-col items-center gap-12'>
             <h3 className='text-2xl font-bold font-principal'>Seguinos</h3>
             <a href='https://www.instagram.com/bpink.cba/' target="_blank" className='relative w-fit hover:scale-110 hover:cursor-pointer transition-all ease-in-out duration-200'>
-                <img src={backgroundImage} className='absolute top-[47%] left-[53%] -translate-x-1/2 -translate-y-1/2 z-10'/>
-                <img src={backgroundImage} className='z-50 filter brightness-50'/>
-                <BsInstagram className='absolute top-[47%] left-[53%] -translate-x-1/2 -translate-y-1/2 z-50 text-white opacity-80 text-[75px]'/>
+                <img src={backgroundImage} className='filter brightness-50'/>
+                <img src={backgroundImage} className='absolute top-[47%] left-[53%] -translate-x-1/2 -translate-y-1/2'/>
+                <BsInstagram className='absolute top-[47%] left-[53%] -translate-x-1/2 -translate-y-1/2 text-white opacity-80 text-[75px]'/>
             </a>
         </div>
     )
