91a7d9c3ba84c15992282f466bac3daaf791a27e
alejandroruizdiaz <alee_ruizdiaz@hotmail.com>
2023-03-09 16:41:54 -0300

    Adding footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
     return (
         <div className="App">
-            <Navbar />
+            <Footer/>
         </div>
     );
 }
