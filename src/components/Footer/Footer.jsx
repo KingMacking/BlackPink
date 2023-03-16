fe4207be87d978e66dc4c28f2ed5106d5c09facc
alejandroruizdiaz <alee_ruizdiaz@hotmail.com>
2023-03-16 17:20:54 -0300

    styles howtowuy

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,27 +4,31 @@ import { SiGooglemaps } from "react-icons/si"
 
 const Footer = () => {
     return (
-        <div className="p-20 bg-black lg:flex lg:justify-around">
+        <div className="lg:p-20 p-16 bg-black lg:flex lg:justify-around">
             <div className="flex flex-col justify-center text-white lg:justify-start">
                 <img src={logo} alt="logo"/>
                 <p className="flex justify-center mt-2 lg:mt-4 lg:justify-start">© Blackp:nk 2023</p>
             </div>
-            <div className="flex flex-col items-center text-white lg:items-start">
-                <a href="https://goo.gl/maps/PA61pzkBY241snXXA" target="_blank" className="mt-4 text-xl font-bold lg:mt-0 hover:underline underline-offset-4 flex">
-                    <SiGooglemaps className="text-pinkWhite"/>
-                    <p className="mx-2">Corrientes 226, Córdoba Capital, Argentina</p>
-                </a>
-                <p className="my-1 mt-4 lg:my-2 lg:mt-4">Hararios: </p>
-                <p className="my-1">Lunes a viernes de 9hs a 18hs</p>
-                <p className="my-1">Sábado de 9hs a 14hs</p>
+            <div className="flex justify-center lg:pt-0 pt-5">
+                <div className="mx-2">
+                    <SiGooglemaps className="text-pinkLogo lg:text-[2.5rem]  text-[2rem]"/>
+                </div>
+                <div className="flex flex-col items-start  text-white lg:items-start">
+                    <a href="https://goo.gl/maps/PA61pzkBY241snXXA" target="_blank" className="mt-0  text-xl font-bold lg:mt-0 hover:underline underline-offset-4 flex">
+                        <p>Corrientes 226, Córdoba Capital, Argentina</p>
+                    </a>           
+                    <p className="my-1 mt-4 lg:my-2 lg:mt-4">Hararios: </p>
+                    <p className="my-1">Lunes a viernes de 9hs a 18hs</p>
+                    <p className="my-1">Sábado de 9hs a 14hs</p>                   
+                </div>
             </div>
-            <div className="flex flex-col text-white">
-                <h1 className="flex justify-center mt-5 text-xl font-bold lg:mt-0 lg:justify-start">Informacion</h1>
+            <div className="flex flex-col text-white ">
                 <div className="flex flex-col items-center lg:items-start">
-                    <Link className="my-1 mt-4 lg:my-2 lg:mt-4 hover:underline underline-offset-4" to={'/'}>Preguntas frecuentes</Link>
-                    <Link className="my-1 lg:my-2 hover:underline underline-offset-4" to={'/'}>Métodos de pago</Link>
-                    <Link className="my-1 lg:my-2 hover:underline underline-offset-4" to={'/'}>Envíos</Link>
-                    <Link className="my-1 lg:my-2 hover:underline underline-offset-4" to={'/'}>Cambios y devoluciones</Link>
+                <h1 className="flex justify-start mt-5 text-xl font-bold lg:mt-0 lg:justify-start">Informacion</h1>
+                    <Link className="my-1 mt-4 lg:my-2 lg:mt-4 hover:underline underline-offset-4 text-pinkLogo" to={'/'}>Preguntas frecuentes</Link>
+                    <Link className="my-1 lg:my-2 hover:underline underline-offset-4 text-pinkLogo" to={'/'}>Métodos de pago</Link>
+                    <Link className="my-1 lg:my-2 hover:underline underline-offset-4 text-pinkLogo" to={'/'}>Envíos</Link>
+                    <Link className="my-1 lg:my-2 hover:underline underline-offset-4 text-pinkLogo" to={'/'}>Cambios y devoluciones</Link>
                 </div>
             </div>
         </div>
--- src/components/Pages/HowToBuy/PaymentMethods/PaymentMethods.jsx
+++ src/components/Pages/HowToBuy/PaymentMethods/PaymentMethods.jsx
@@ -1,17 +1,17 @@
 const PaymentMethods = () => {
     return (
         <div className="flex flex-col ">
-            <div className="mb-10 text-4xl font-bold text-center font-principal">
+            <div className="mb-10 lg:text-4xl text-[1.4rem] font-bold text-center font-principal">
                 <h1>Métodos de pago</h1>
             </div>
-            <div className="flex flex-col">
-                <div className="flex flex-col items">
-                    <h1 className="text-2xl font-bold font-principal">Mayorista</h1>
-                    <p className="text-xl">Efectivo o depósito por transferencia</p>
+            <div className="flex flex-col lg:pt-10">
+                <div className="flex flex-col justify-center items-center lg:pb-10 pb-5">
+                    <h1 className="lg:text-2xl text-[1.2rem] font-bold font-principal lg:pb-5 pb-3">Mayorista</h1>
+                    <p className="lg:text-xl text-[1rem]">Efectivo o depósito por transferencia</p>
                 </div>
-                <div className="my-10">
-                    <h1 className="text-2xl font-bold font-principal">Minorista</h1>
-                    <p className="text-xl">Efectivo, dpebito o crpedito (con un 10% de recargo).</p>
+                <div className="lg:my-10 my-6 lg:px-0 px-10 flex flex-col items-center">
+                    <h1 className="lg:text-2xl text-[1.2rem] font-bold font-principal pb-5">Minorista</h1>
+                    <p className="lg:text-xl text-[1rem] ">Efectivo, dpebito o crpedito (con un 10% de recargo).</p>
                 </div>
             </div>
         </div>
