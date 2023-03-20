da58f815d4581fe35be956f66e9c7f17db1ba3ab
alejandroruizdiaz <alee_ruizdiaz@hotmail.com>
2023-03-20 15:43:55 -0300

    Adding image products

--- src/components/Pages/Products/FilterBySelection/FilterBySelection.jsx
+++ src/components/Pages/Products/FilterBySelection/FilterBySelection.jsx
@@ -1,6 +1,6 @@
 const FilterBySelection = ({setOrder}) => {
     return (
-        <select className="px-4 pt-1 pb-2 text-white bg-black rounded-lg justify-self-start" onChange={(e) => setOrder(e.target.value)}>
+        <select className="px-4 pt-2 pb-2 text-white bg-pinkCta rounded-lg justify-self-start" onChange={(e) => setOrder(e.target.value)}>
             <option value="">Ordenar por</option>
             <option value="priceAsc">Precio ascendente</option>
             <option value="priceDesc">Precio descendente</option>
